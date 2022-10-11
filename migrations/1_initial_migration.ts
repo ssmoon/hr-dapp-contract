7198d11177c05296be8c626085511547f7aa484f
jinjing <1131726@qq.com>
2022-10-11 09:59:00 +0800

    staged

--- migrations/1_initial_migration.ts
+++ migrations/1_initial_migration.ts
@@ -17,45 +17,45 @@ const CertificateStorageContract = artifacts.require('CertificateStorage')
 const migration: Truffle.Migration = async function (deployer, _, accounts) {
   const account = accounts[0];
 
-  deployer.deploy(ProxyContract)
+  await deployer.deploy(ProxyContract)
   const ProxyDeployed = await ProxyContract.deployed();
 
   // contract discovery center
-  deployer.deploy(DispatcherContract, ProxyDeployed.address);
+  await deployer.deploy(DispatcherContract, ProxyDeployed.address);
   const DispatcherDeployed = await DispatcherContract.deployed();
 
   // proxy delegate impl
-  deployer.deploy(FacadeContract, DispatcherDeployed.address, account);
-  DispatcherDeployed.importAddress("Facade", FacadeContract.address)
+  await deployer.deploy(FacadeContract, DispatcherDeployed.address, account);
+  await DispatcherDeployed.importAddress(web3.utils.fromAscii("Facade"), FacadeContract.address)
 
   // set current proxyed impl
-  ProxyDeployed.setImplementation(FacadeContract.address);
+  await ProxyDeployed.setImplementation(FacadeContract.address);
 
   // deploy and register all other contracts
 
-  deployer.deploy(CareerServiceContract, DispatcherDeployed.address);
-  DispatcherDeployed.importAddress("CareerService", CareerServiceContract.address);
+  await deployer.deploy(CareerServiceContract, DispatcherDeployed.address);
+  await DispatcherDeployed.importAddress("CareerService", CareerServiceContract.address);
 
-  deployer.deploy(CareerStorageContract, DispatcherDeployed.address);
-  DispatcherDeployed.importAddress("CareerStorage", CareerStorageContract.address);
+  await deployer.deploy(CareerStorageContract, DispatcherDeployed.address);
+  await DispatcherDeployed.importAddress("CareerStorage", CareerStorageContract.address);
 
-  deployer.deploy(UserServiceContract, DispatcherDeployed.address);
-  DispatcherDeployed.importAddress("UserService", UserServiceContract.address);
+  await deployer.deploy(UserServiceContract, DispatcherDeployed.address);
+  await DispatcherDeployed.importAddress("UserService", UserServiceContract.address);
 
-  deployer.deploy(UserStorageContract, DispatcherDeployed.address);
-  DispatcherDeployed.importAddress("UserStorage", UserStorageContract.address);
-  
-  deployer.deploy(WorkerServiceContract, DispatcherDeployed.address);
-  DispatcherDeployed.importAddress("WorkerService", WorkerServiceContract.address);
+  await deployer.deploy(UserStorageContract, DispatcherDeployed.address);
+  await DispatcherDeployed.importAddress("UserStorage", UserStorageContract.address);
 
-  deployer.deploy(WorkerStorageContract, DispatcherDeployed.address);
-  DispatcherDeployed.importAddress("WorkerStorage", WorkerStorageContract.address);
+  await deployer.deploy(WorkerServiceContract, DispatcherDeployed.address);
+  await DispatcherDeployed.importAddress("WorkerService", WorkerServiceContract.address);
 
-  deployer.deploy(CertificateServiceContract, DispatcherDeployed.address);
-  DispatcherDeployed.importAddress("CertificateService", CertificateServiceContract.address);
+  await deployer.deploy(WorkerStorageContract, DispatcherDeployed.address);
+  await DispatcherDeployed.importAddress("WorkerStorage", WorkerStorageContract.address);
 
-  deployer.deploy(CertificateStorageContract, DispatcherDeployed.address);
-  DispatcherDeployed.importAddress("CertificateStorage", CertificateStorageContract.address);
+  await deployer.deploy(CertificateServiceContract, DispatcherDeployed.address);
+  await DispatcherDeployed.importAddress("CertificateService", CertificateServiceContract.address);
+
+  await deployer.deploy(CertificateStorageContract, DispatcherDeployed.address);
+  await DispatcherDeployed.importAddress("CertificateStorage", CertificateStorageContract.address);
 }
 
 module.exports = migration
