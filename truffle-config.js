1a417f11c6bd4bf476d91d171b882707ff09a1f9
jinjing <1131726@qq.com>
2022-10-11 10:34:12 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -7,11 +7,6 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
-    },
-    loc_8545_8545: {
-      network_id: "*",
-      port: 8545,
-      host: "127.0.0.1"
     }
   },
   mocha: {},
